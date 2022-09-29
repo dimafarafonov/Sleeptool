5147a893ed716199ec6395cadb3234c1ea6fe807
d.farafonov <d.farafonov@omo.systems>
2022-09-29 21:21:02 +0300

    install more modules/design wallpaper/ config babel

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,49 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { Text } from 'react-native';
+
+import styled from 'styled-components/native';
+
+import LinearGradient from 'react-native-linear-gradient';
+
+import { pallete } from 'utils/brand-book';
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>future start</Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <SafeAreaViewContainerTop />
+      <SafeAreaViewContainerBottom>
+        <Container
+          colors={[
+            pallete.midnightBlueGreen,
+            pallete.wildBlueYonder,
+            pallete.midnightBlueGreen,
+          ]}
+          locations={[0.15, 0.7, 0.15]}>
+          <Text>future start</Text>
+        </Container>
+      </SafeAreaViewContainerBottom>
+    </>
   );
 };
+
+const Container = styled(LinearGradient)`
+  padding: 15px;
+  display: flex;
+  border-color: black;
+  border-width: 2px;
+  height: 100%;
+`;
+
+const SafeAreaViewContainerTop = styled.SafeAreaView`
+  background-color: ${pallete.midnightBlueGreen};
+  display: flex;
+  flex: 0;
+`;
+
+const SafeAreaViewContainerBottom = styled.SafeAreaView`
+  background-color: ${pallete.wildBlueYonder};
+  display: flex;
+  flex: 1;
+`;
 
 export { HomeScreen };
---
+++ src/utils/brand-book.ts
@@ -0,0 +1,22 @@
+const pallete = Object.freeze({
+  grannySmithApple: '#ADF7B6',
+  yellowCrayola: '#FFEE93',
+  wildBlueYonder: '#8FA6CB',
+  silver: '#C0C0C0',
+  mountbattenPink: '#9E768F',
+  yellowGreen: '#91CB3E',
+  blurredWhite: '#FCFCFC',
+  mauvelous: '#E4959E',
+  rosyBrown: '#E4959E',
+  violentBlueCrayola: '#6369D1',
+  pastelPink: '#DA9F93',
+  champagnePink: '#EBD4CB',
+  silverPink: '#B79D94',
+  puce: '#C6878F',
+  midnightBlueGreen: '#8CDFD6',
+  beauBlue: '#8CDFD6',
+  deepTaupe: '#88665D',
+  mintCream: '#F2F7F2',
+});
+
+export { pallete };
