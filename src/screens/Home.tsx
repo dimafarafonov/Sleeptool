2e25b3a6617cc7243fcf03c8dd6b13e3a53ce6a6
d.farafonov <d.farafonov@omo.systems>
2022-09-29 20:22:06 +0300

    config project add styled components

---
+++ src/screens/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>future start</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export { HomeScreen };
