537b09ac0eb7a12e945f67d6f9b004d1aff6af40
d.farafonov <d.farafonov@omo.systems>
2022-11-23 17:48:02 +0200

    make request finally work with fetch api

--- src/api/postImage.ts
+++ src/api/postImage.ts
@@ -1,26 +1,18 @@
 import { Images } from 'assets/index';
 
 const postImage = async () => {
-  //Check if any file is selected or not
-  if (true) {
-    //If file selected then create FormData
-
-    // const file = {
-    //   uri: filePath,
-    //   type: fileType,
-    //   name: filename,,
-    // };
-    const data = new FormData();
-    data.append('randomtext', 'Image Upload');
-    data.append('file', Images.mock);
-    let res = await fetch('http://10.0.2.2:8000/image/upload', {
-      method: 'POST',
-      body: data,
-    }).catch(err => console.log('err', err));
-  } else {
-    //if no file selected the show alert
-    console.log('Please Select File first');
-  }
+  const data = new FormData();
+  data.append('randomtext', 'Image Upload');
+  data.append('file', Images.mock);
+  await fetch('http://10.0.2.2:8000/image/upload', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+    body: data,
+  }).catch(err => console.log('err', err));
 };
 
 export { postImage };
+
+// TODO: bring react-native-image-picker into the project
